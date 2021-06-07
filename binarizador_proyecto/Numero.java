80e41297dc274f7c0a430f0fbb9cc867f40a31e9
Javier Vásquez <javier.jvasquez@gmail.com>
2021-06-07 13:46:55 -0600

    comentarizacion del codigo v1

--- binarizador_proyecto/Numero.java
+++ binarizador_proyecto/Numero.java
@@ -1,23 +1,31 @@
 
 /**
- * Write a description of class binarizador here.
+ * ejemplo de programa recursivo
+ * convierte entero en representacion binaria
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author javier vásquez
+ * @version 1 del 7 de junio 2021
  */
 public class Numero
 {
+    /**
+     * @param n=numero en proceso de conversion a binario
+     * @return hilera que representa num binario
+       */
+    
     public String toBinary(int n){
+        //condicion de parada
         if (n<=1)
             return ""+n;
         else
+        //llamado recursivo. Concatenacion por backtracking
             return toBinary(n/2)+n%2;
     }
-    
+
     public static void main (String a[]){
         //Test de funcionalidad
         Numero n=new Numero();
-        
+
         System.out.println("13 en binario es "+n.toBinary(13));
     }
 }
